e3952f9b8936df3367341421148d71a48502e06c
Hicran Apaydın <hicran.apaydn@gmail.com>
2025-05-12 12:11:52 +0300

    register page added

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,7 +6,6 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,45 +13,56 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { loginSchema } from "@/lib/validation"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import Link from "next/link"
 
 
-function onSubmit(values: z.infer<typeof loginSchema>) {
-    console.log(values)
-  }
 
 
 
 const LoginPage = () => {
 
+    const router = useRouter();
+    const [error, setError] = useState("")
+
+   function onSubmit(values: z.infer<typeof loginSchema>) {
+        console.log(values)
+      }
+    
+
     const form = useForm<z.infer<typeof loginSchema>>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
-          username: "",
+          email: "",
+          password: "",
         },
       })
 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-amber-50  " >
-      <div className="flex flex-col items-center justify-center w-full max-w-md p-4 bg-white rounded-lg shadow-md ">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-amber-50 p-4 " >
+      <div className="flex flex-col items-center justify-center w-full max-w-md px-8 pt-6 pb-8 bg-white rounded-lg shadow-md ">
         <h1 className="text-2xl font-bold">Login</h1>
         <p className="text-sm text-gray-500 p-4">Please enter your credentials</p>
+        {error && (
+          <div className="mb-4 text-red-500">
+            {error}
+          </div>
+        )}
      
    
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>Email</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="dareyn@expample.com" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -64,16 +74,19 @@ const LoginPage = () => {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="dareyn.@" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your password.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <div className="flex flex-row items-center justify-between">
+        <Button type="submit">Login</Button>
+        <Link href="/register" className="text-sm font-bold inline-block align-baseline text-blue-500 ">
+          Register
+        </Link>
+        </div>
+        
       </form>
     </Form>
   
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,14 +1,15 @@
+
 import {z} from "zod"
 
 
 export const loginSchema = z.object({
-  username: z
+  email: z
     .string()
     .min(2, {
-      message: "Username must be at least 2 characters.",
+      message: "email must be at least 2 characters.",
     })
     .max(20, {
-      message: "Username must be at most 20 characters.",
+      message: "email must be at most 20 characters.",
     }),
   password: z
     .string()
@@ -19,3 +20,41 @@ export const loginSchema = z.object({
       message: "Password must be at most 100 characters.",
     }),
 })
+
+export const registerSchema = z
+  .object({
+    email: z
+      .string()
+      .min(2, {
+        message: "email must be at least 2 characters.",
+      })
+      .max(20, {
+        message: "email must be at most 20 characters.",
+      }),
+    password: z
+      .string()
+      .min(8, {
+        message: "Password must be at least 8 characters.",
+      })
+      .max(100, {
+        message: "Password must be at most 100 characters.",
+      }),
+      firstName: z
+      .string()
+      .min(2, {
+        message: "firstName must be at least 2 characters.",    
+      })
+      .max(20, {
+        message: "firstName must be at most 20 characters.",
+      }),
+      lastName: z
+      .string()
+      .min(2, {
+        message: "lastName must be at least 2 characters.",   
+      })
+      .max(20, {
+        message: "lastName must be at most 20 characters.",
+      }),                
+
+
+    })
